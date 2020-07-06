3a92856f938da8bd6d27aef4ee7bce9984103ba4
fgsantosti <fgsantos.ti@gmail.com>
2020-07-06 10:53:05 -0300

    Tela Questionario e logica

--- lib/screens/tela-questionario.dart
+++ lib/screens/tela-questionario.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:smccovid/components/item_pergunta.dart';
-
+import 'package:smccovid/models/pergunta.dart';
 import '../components/custom-botton.dart';
-import '../constants/constants.dart';
 import '../constants/constants.dart';
 import '../constants/constants.dart';
 
@@ -12,142 +11,144 @@ class Questionario extends StatefulWidget {
   _QuestionarioState createState() => _QuestionarioState();
 }
 
-
 var perguntasTeste = [
   {
-    'pergunta':'Alguém de sua família esta com gripe?',
-    'valor':false
+    'pergunta': 'Você costuma realizar viagens em transporte coletivo?',
+    'valor': false
   },
   {
-    'pergunta':'Teve febre nos últimos 5 dias?',
-    'valor':false
+    'pergunta':
+        'Nos últimos 14 dias, você esteve em contato próximo com pessoa suspeita ou confirmada da COVID-19?',
+    'valor': false
   },
   {
-    'pergunta':'Teve contato com pessoas que estiveram COVID 19?',
-    'valor':false
+    'pergunta': 'Sente ou sentiu algum problema de saúde nos últimos 7 dias?',
+    'valor': false
   },
   {
-    'pergunta':'Possui problemas como, coração, obesidade, etc. ',
-    'valor':false
+    'pergunta':
+        'Você apresentou ou apresenta algum sinal/sintoma de síndrome gripal (febre, tosse, congestão nasal, dor garganta, dor cabeça, dor no corpo, falta de ar, perda do paladar, perda do olfato, diarreia, enjoo), nos últimos 14 dias?',
+    'valor': false
   },
-
-
+  {'pergunta': 'Realizou consulta médica?', 'valor': false},
+  {'pergunta': 'Realizou teste para a COVID-19?', 'valor': false},
+  {
+    'pergunta':
+        'Você esteve ou está em isolamento social com monitoramento da equipe de saúde do seu município, ou internação hospitalar?',
+    'valor': false
+  },
+  {
+    'pergunta': 'Possui problemas como, coração, obesidade, etc. ',
+    'valor': true
+  },
 ];
-
-
-
-
-
-
+var lsb = [];
 
 class _QuestionarioState extends State<Questionario> {
   List _respostar = List();
 
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    int taml = perguntasTeste.length;
+    for (var i = 0; i < taml; i++) {
+      lsb.add(false);
+    }
+    print(lsb);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(     
-
+    return Scaffold(
       body: SingleChildScrollView(
         child: Stack(
-        children: [
-          Container(
-            color: cor_base,
-            height: MediaQuery.of(context).size.width/2.5,
-          ),
-
-          SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              color: cor_base,
+              height: MediaQuery.of(context).size.width / 2.5,
+            ),
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
                   children: [
-                    Text('Questionário', style: TextStyle(fontWeight: FontWeight.w900, fontSize: 30, color: Colors.white),),
-                    
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Questionário',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                              fontSize: 30,
+                              color: Colors.white),
+                        ),
+                      ],
+                    ),
+                    Center(
+                      child: Container(
+                        margin: EdgeInsets.only(
+                            top: MediaQuery.of(context).size.width / 30),
+                        height: MediaQuery.of(context).size.width / 2.5,
+                        width: MediaQuery.of(context).size.width / 2.5,
+                        decoration: BoxDecoration(
+                            color: cor_base, shape: BoxShape.circle),
+                        child: Container(
+                          padding: EdgeInsets.all(20),
+                          margin: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                              color: Colors.white, shape: BoxShape.circle),
+                          child: Icon(
+                            Icons.message,
+                            size: 80,
+                            color: cor_base,
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    for (int i = 0; i < perguntasTeste.length; i++)
+                      ItemPergunta(
+                        pergunta: perguntasTeste[i]['pergunta'],
+                        valor: perguntasTeste[i]['valor'],
+                        onPressed: () {
+                          if (perguntasTeste[i]['valor']) {
+                            perguntasTeste[i]['valor'] = false;
+                          } else {
+                            perguntasTeste[i]['valor'] = true;
+                          }
+                          setState(() {});
+                        },
+                      ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    CustomButton(
+                      color: cor_base,
+                      cor_indicador: Colors.white,
+                      title: Text(
+                        'Concluir',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {
+                        print('aqui');
+                        print(lsb);
+                        print('aqui');
+                        print(perguntasTeste);
+                      },
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
                   ],
                 ),
-
-                Center(
-                  child: Container(                   
-                    margin: EdgeInsets.only(top: MediaQuery.of(context).size.width/30),               
-                    height: MediaQuery.of(context).size.width/2.5,
-                    width: MediaQuery.of(context).size.width/2.5,
-                    decoration: BoxDecoration(
-                      color: cor_base, 
-                      shape: BoxShape.circle                      
-                    ),
-
-
-                    child: Container( 
-                    padding: EdgeInsets.all(20),
-                    margin: EdgeInsets.all(5),  
-                    decoration: BoxDecoration(
-                      color: Colors.white, 
-                      shape: BoxShape.circle                      
-                    ),
-                    child: Icon(Icons.message, size: 80, color: cor_base,),
-
-
-                    
-
-                  ),
-
-                  
-
-
-
-                    
-
-                  ),
-                ), 
-                SizedBox(height: 50,),
-
-
-                for (var item in perguntasTeste)
-                ItemPergunta(
-                  pergunta: item['pergunta'],
-                  valor: _respostar.contains(item),
-                  onPressed: (){
-                    if (_respostar.contains(item)) {
-                      _respostar.remove(item);
-                    } else {
-                      _respostar.add(item);
-                    }
-                    
-                    setState(() {
-                      
-                    });
-                  },                  
-                ), 
-
-
-                SizedBox(height: 50,),
-                CustomButton(
-                  color: cor_base,
-                  cor_indicador: Colors.white,
-                  title: Text('Concluir', style: TextStyle(color: Colors.white),),
-                  onPressed: (){
-
-                  },
-                ),
-                SizedBox(height: 50,),
-
-                
-
-
-
-              ],
-          ),
-            ),
-          )  
-        
-
-        ],
-      ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
